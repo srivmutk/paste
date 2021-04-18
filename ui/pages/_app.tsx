df0d8dfebe451d6ee90394bf5388dbbb23e21b89
Sysnomid <sam@sysnomid.com>
2021-04-18 11:06:19 -0400

    Add lang=en

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -9,7 +9,7 @@ const SERVER_URL = process.env.SERVER_URL as string;
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <html lang="en">
       <SWRConfig
         value={{
           refreshInterval: 3000,
@@ -49,7 +49,7 @@ const App = ({ Component, pageProps }: AppProps) => {
           <Footer />
         </div>
       </SWRConfig>
-    </>
+    </html>
   );
 };
 
