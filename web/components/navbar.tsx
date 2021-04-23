1ce873e21669f01235ebbebe8e9634b98fc64188
Sysnomid <52793419+Sysnomid@users.noreply.github.com>
2021-04-23 18:09:16 -0400

    Fix Wrapping on Mobile

--- web/components/navbar.tsx
+++ web/components/navbar.tsx
@@ -91,17 +91,7 @@ export default function Navbar() {
           <div className="block rounded w-full">
             <div className="flex flex-col lg:hidden xl:hidden pb-3 pt-3 pr-3 pl-3">
               <NavbarItem link="/" sm={true}>
-                <a className="flex">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 48 48"
-                    className="fill-current text-white w-8 pr-2"
-                  >
-                    <path d="M6 34.5v7.5h7.5l22.13-22.13-7.5-7.5-22.13 22.13zm35.41-20.41c.78-.78.78-2.05 0-2.83l-4.67-4.67c-.78-.78-2.05-.78-2.83 0l-3.66 3.66 7.5 7.5 3.66-3.66z" />
-                    <path d="M0 0h48v48h-48z" fill="none" />
-                  </svg>
-                  New Paste
-                </a>
+                New Paste
               </NavbarItem>
               <NavbarItem link="/about" sm={true}>
                 About
