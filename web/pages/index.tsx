48e8fc384d6c7640fd14db0e73eb5f12c36331be
Sysnomid <52793419+Sysnomid@users.noreply.github.com>
2021-04-27 18:33:39 -0400

    Added Loading Animation for Index page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -56,7 +56,35 @@ const IndexPage = () => {
     return (
       <>
         <Layout>
-          <span>Loading ...</span>
+          {" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            style={{ margin: "auto", background: "none" }}
+            width="100"
+            height="200"
+            display="block"
+            preserveAspectRatio="xMidYMid"
+            viewBox="0 0 100 100"
+          >
+            <circle
+              cx="50"
+              cy="50"
+              r="35"
+              fill="none"
+              stroke="#e15b64"
+              strokeDasharray="164.93361431346415 56.97787143782138"
+              strokeWidth="10"
+            >
+              <animateTransform
+                attributeName="transform"
+                dur="1s"
+                keyTimes="0;1"
+                repeatCount="indefinite"
+                type="rotate"
+                values="0 50 50;360 50 50"
+              ></animateTransform>
+            </circle>
+          </svg>
         </Layout>
       </>
     );
